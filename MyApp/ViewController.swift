4344d32900e5c1667cafc13ce843bd8612f46116
Mishenina Evgenia <eamishenina@gmail.com>
2026-03-30 14:48:23 +0300

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by g1nz4 on 30.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
